5f89f22579a750d012814bef936701334b424a99
xyd-shawn <yadong.xu@outlook.com>
2018-05-09 20:42:50 +0800

    fix bugs and add model conv2d

--- src/configs.py
+++ src/configs.py
@@ -7,6 +7,7 @@ config1 = {
         'n_classes': 41,
         'use_mfcc': False,
         'n_mfcc': 20,
+        'use_folds': False,
         'n_folds': 10,
         'learning_rate': 0.001,
         'max_epochs': 50,
@@ -14,6 +15,8 @@ config1 = {
         'audio_pad_method': 'constant',
         'data_dir': '../data/',
         'log_dir': '../logs/',
-        'tmp_dir': '../tmp/'
+        'tmp_dir': '../tmp/',
+        'model_name': 'model_1',
+        'run_time': 1
 }
 
